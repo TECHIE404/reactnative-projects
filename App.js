6b9976d10c58c488b1f192638716d566dfbc9a07
RAHUL KUMAR <techierahul404@gmail.com>
2020-05-29 13:37:48 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
 import React, {useState} from 'react';
 import { StyleSheet, Text, View ,Button} from 'react-native';
 
-export default function App() {
-  const [outputText, setOutputText]= useState('Hello Cha, welcom to my first web reactnative application using expo cli !');
-  return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title="Say hi !" onPress={()=>setOutputText('The text is just changed!, this one is a simple')}/>
-    </View>
-  );
+export default class App extends React.Component {
+  state={
+    myState: 'This is my state '
+  }
+ 
+  render(){
+    return (
+      <View style={styles.container}>
+          <View style={styles.redbox}/>
+          <View style={styles.bluebox}/>
+          <View style={styles.blackbox}/>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexDirection: 'column',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'grey',
+    height:600
+  },
+  redbox:{
+    width:100,
+    height:100,
+    backgroundColor:'red'
+  },
+  bluebox:{
+    width:100,
+    height:100,
+    backgroundColor:'blue'
+  },
+  blackbox:{
+    width:100,
+    height:100,
+    backgroundColor:'black'
   },
 });
